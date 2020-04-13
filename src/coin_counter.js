98cd8c13822b6b0336540a922a60fbd18fcb90fc
Patrick Kille <pr.kille@gmail.com>
2020-04-13 14:21:53 -0700

    add in full functionality, has undefined bug
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: acolistro <arcolistro@gmail.com>

--- src/coin_counter.js
+++ src/coin_counter.js
@@ -5,49 +5,47 @@ export function CoinCounter(price, cash) {
   this.coins = [.25, .10, .05, .01];
 }
 
-CoinCounter.prototype.toCoins = function(change, coins) {
-  if(isNaN(change)) {
-    return;
+CoinCounter.prototype.toCoins = function() {
+  if(isNaN(this.change)) {
+    return "blah";
   }
-  if (change === 0) {
+  if (this.change === 0) {
     return [];
   } else {
-    if (change >= coins[0]) {
-      let remains = (change - coins[0]);
-      return [coins[0]].concat( this.toCoins(remains, coins));
+    if (this.change >= this.coins[0]) {
+      this.change = (this.change - this.coins[0]);
+      return [this.coins[0]].concat(this.toCoins());
     } else {
       coins.shift();
-      return this.toCoins(change, coins);
+      return this.toCoins(this.change, this.coins);
     }
   }
 }
 
-// CoinCounter.prototype.countChange = function() {
-//   if(isNaN(this.change)) {
-//     return;
-//   }
-//   if(this.change <= 0) {
-//     return "";
-//   }
-//   else {
-//     if(this.change >= .25){
-//       const quarters = this.change/.25;
-//       this.change = this.change%.25;
-//       return this.countChange() + `Quarters: ${quarters}`;
-//     }
-    // else if(this.change >= .1){
-    //   const change2 = this.change%.25;
-    //   const dimes = change2/.1;
-    //   return CoinCounter.countChange(this.change%.1) + `Dimes: ${dimes}`;
-    // }
-    // // else if(this.change >= .05){
-    //   const nickels = this.change/.05;
-    //   console.log("Nickels: " + nickels);
-    //   return CoinCounter.countChange(this.change%.05) + "";
-    // }
-    // else(this.change >= .01)
-    //   const pennies = this.change/.01;
-    //   console.log("Pennies: " + pennies);
-    //   return CoinCounter.countChange(this.change%.01) + "";
-  // }
-// }
+CoinCounter.prototype.countChange = function() {
+  if(isNaN(this.change)) {
+    return "blah";
+  }
+  if(this.change === 0) {
+    return "";
+  }
+  else {
+    if(this.change >= .25){
+      const quarters = Math.floor(this.change/.25);
+      this.change = this.change - quarters*.25;
+      return this.countChange() + ` Quarters: ${quarters}`;
+    } else if (this.change >= .1) {
+      const dimes = Math.floor(this.change/.10);
+      this.change = this.change - dimes*.10;
+      return this.countChange() + ` Dimes: ${dimes}`;
+    } else if (this.change >= .05) {
+      const nickels = Math.floor(this.change/.05);
+      this.change = this.change - nickels*.05;
+      return this.countChange() + ` Nickels: ${nickels}`;
+    } else if (this.change >= .01) {
+      const pennies = Math.round(this.change*100);
+      this.change = 0;
+      return this.countChange() + ` Pennies: ${pennies}`;
+    }
+  }
+}
