6bd095e5fbbb581713bc1166c8f519f46528efde
Patrick Kille <pr.kille@gmail.com>
2020-04-13 14:21:15 -0700

    add in dime test
    Co-authored-by: acolistro <arcolistro@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- __tests__/coin_counter.test.js
+++ __tests__/coin_counter.test.js
@@ -1,10 +1,24 @@
 import { CoinCounter } from './../src/coin_counter.js';
 
 describe ('CoinCounter', () => {
-  test ('should correctly return the number of quarters', () => {
-    var coinCounter = new CoinCounter(1.75, 2.00);
+  test ('should correctly return an array of coin values', () => {
+    var coinCounter = new CoinCounter(1.75, 2.25);
     var quarters = coinCounter.toCoins();
     console.log(quarters);
-    expect (coinCounter.toCoins()).toEqual()
+    expect (quarters).toEqual([.25, .25])
+  });
+
+  test ('returns string of number of coins', () => {
+    var coinCounter = new CoinCounter(1.75, 2.25);
+    var quarters = coinCounter.countChange();
+    console.log(quarters);
+    expect(quarters).toEqual(" Quarters: 2")
+  });
+
+  test ('returns string of number of quarters and dimes', () => {
+    var coinCounter = new CoinCounter(1.75, 2.35);
+    var coins = coinCounter.countChange();
+    console.log(coins);
+    expect(coins).toEqual(" Dimes: 1Quarters: 2")
   });
 })
